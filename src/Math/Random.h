d54b66e36db262a064850d234676fcc798fcd2a6
DeLaPF <de.alafia@gmail.com>
2023-02-16 16:21:17 +0000

    Make RandomDouble thread safe

--- src/Math/Random.h
+++ src/Math/Random.h
@@ -3,7 +3,12 @@
 #include <random>
 
 inline double RandomDouble() {
-    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
-    static thread_local std::mt19937 generator;
+    std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    thread_local std::mt19937 generator(std::random_device{}());
     return distribution(generator);
+}
+
+inline double RandomDouble(double min, double max) {
+    // Returns a random real in [min,max).
+    return min + ((max - min) * RandomDouble());
 }
