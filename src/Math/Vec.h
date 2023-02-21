3193e0e24e58113c9ee81f38dcf4ddf8ffa60021
DeLaPF <de.alafia@gmail.com>
2023-02-21 15:12:02 +0000

    Remove conditional statements for math between Vecs of different sizes
    May want to figure out a less condition-heavy way make sure indices that were zero stay zero

--- src/Math/Vec.h
+++ src/Math/Vec.h
@@ -6,51 +6,24 @@
 
 template <int SIZE>
 class Vec {
-    static_assert(SIZE > 1 && SIZE <= 4, "Error: Vec<SIZE>, SIZE must be 2, 3, or 4");
 public:
+static_assert(SIZE > 1 && SIZE <= 4, "Error: Vec<SIZE>, SIZE must be 2, 3, or 4");
     Vec() : X(0), Y(0), Z(0), W(0) { vec = &X; }
-    Vec(double v) : X(v),
-            Y(SIZE > 1 ? v : 0),
-            Z(SIZE > 2 ? v : 0),
-            W(SIZE > 3 ? v : 0) { vec = &X; }
+    Vec(double v) : X(v), Y(v), Z(v), W(v) { vec = &X; }
     Vec(double x, double y) : X(x), Y(y), Z(0), W(0) { vec = &X; }
-    Vec(double x, double y, double z) : X(x),
-            Y(SIZE > 1 ? y : 0),
-            Z(SIZE > 2 ? z : 0), W(0) { vec = &X; }
-    Vec(double x, double y, double z, double w) : X(x),
-            Y(SIZE > 1 ? y : 0),
-            Z(SIZE > 2 ? z : 0),
-            W(SIZE > 3 ? w : 0) { vec = &X; }
-    Vec(const Vec& v) : X(v.X),
-            Y(v.size > 1 ? v.Y : 0),
-            Z(v.size > 2 ? v.Z : 0),
-            W(v.size > 3 ? v.W : 0) { vec = &X; }
+    Vec(double x, double y, double z) : X(x), Y(y), Z(z), W(0) { vec = &X; }
+    Vec(double x, double y, double z, double w) : X(x), Y(y), Z(z), W(w) { vec = &X; }
+    Vec(const Vec& v) : X(v.X), Y(v.Y), Z(v.Z), W(v.W) { vec = &X; }
 
-    double length() const {
-        return std::sqrt(lengthSquared());
-    }
-
-    double lengthSquared() const {
-        return dot(*this, *this);
-    }
+    double length() const { return std::sqrt(lengthSquared()); }
+    double lengthSquared() const { return dot(*this, *this); }
 
     double operator[](int i) const { return vec[i]; }
     double& operator[](int i) { return vec[i]; }
-    void operator=(const Vec& v) {
-        X = v.X;
-        Y = SIZE > 1 ? v.Y : 0;
-        Z = SIZE > 2 ? v.Z : 0;
-        W = SIZE > 3 ? v.W : 0;
-    }
-    Vec operator-() const {
-        return Vec<SIZE>(-X, -Y, -Z, -W);
-    }
-    template <int N>
-    Vec& operator+=(const Vec<N>& v) {
-        X += v.X;
-        Y += N > 1 ? v.Y : 0;
-        Z += N > 2 ? v.Z : 0;
-        W += N > 3 ? v.W : 0;
+    void operator=(const Vec& v) { X = v.X, Y = v.Y, Z = v.Z, W = v.W; }
+    Vec operator-() const { return Vec<SIZE>(-X, -Y, -Z, -W); }
+    Vec& operator+=(const Vec& v) {
+        X += v.X, Y += v.Y, Z += v.Z, W += v.W;
         return *this;
     }
     Vec& operator*=(const double t) {
@@ -63,12 +36,8 @@ public:
     }
 public:
     union {
-        struct {
-            double X, Y, Z, W;
-        };
-        struct {
-            double R, G, B, A;
-        };
+        struct { double X, Y, Z, W; };
+        struct { double R, G, B, A; };
     };
     const size_t size = SIZE;
 protected:
@@ -78,20 +47,16 @@ public:
         return u.X * v.X + u.Y * v.Y + u.Z * v.Z + u.W * v.W;
     }
 
-    static inline Vec<SIZE> normalize(Vec<SIZE> v) {
-        return v / v.length();
-    }
+    static inline Vec<SIZE> normalize(Vec<SIZE> v) { return v / v.length(); }
 
     static inline Vec<SIZE> random(double min, double max) {
         return Vec<SIZE>::normalize(Vec<SIZE>(Random::Double(min, max),
-                    SIZE > 1 ? Random::Double(min, max) : 0,
-                    SIZE > 2 ? Random::Double(min, max) : 0,
-                    SIZE > 3 ? Random::Double(min, max) : 0));
+                                              Random::Double(min, max),
+                                              Random::Double(min, max),
+                                              Random::Double(min, max)));
     }
 
-    static inline Vec<SIZE> random() {
-        return Vec<SIZE>::random(-1, 1);
-    }
+    static inline Vec<SIZE> random() { return Vec<SIZE>::random(-1, 1); }
 
     static inline Vec<SIZE> reflect(const Vec<SIZE>& v, const Vec<SIZE>& reflectionAxis) {
         return v - (2 * (Vec<SIZE>::dot(v, reflectionAxis) * reflectionAxis));
