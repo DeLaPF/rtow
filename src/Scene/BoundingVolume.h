6514c180a13bf145389cf7a123ddf2a3850163a8
DeLaPF <de.alafia@gmail.com>
2023-02-25 13:51:12 +0000

    Switched to optimized DidOverlap method and gained another 2.5x

--- src/Scene/BoundingVolume.h
+++ src/Scene/BoundingVolume.h
@@ -15,29 +15,29 @@ public:
     BoundingBox(Vec3 minBound, Vec3 maxBound) : MinBound(minBound), MaxBound(maxBound) {}
 
     virtual bool DoesOverlap(const Ray& ray, double traceDistMin, double traceDistMax) const override {
-        double t0X = std::fmin((MinBound.X - ray.Origin.X) / ray.Direction.X,
-                              (MaxBound.X - ray.Origin.X) / ray.Direction.X);
-        double t1X = std::fmax((MinBound.X - ray.Origin.X) / ray.Direction.X,
-                              (MaxBound.X - ray.Origin.X) / ray.Direction.X);
-        traceDistMin = std::fmax(t0X, traceDistMin);
-        traceDistMax = std::fmin(t1X, traceDistMax);
-        if (traceDistMin >= traceDistMax) { return false; }
+        double invDX = 1.0f / ray.Direction.X;
+        double t0X = (MinBound.X - ray.Origin.X) * invDX;
+        double t1X = (MaxBound.X - ray.Origin.X) * invDX;
+        if (invDX < 0.0f) { std::swap(t0X, t1X); }
+        traceDistMin = t0X > traceDistMin ? t0X : traceDistMin;
+        traceDistMax = t1X < traceDistMax ? t1X : traceDistMax;
+        if (traceDistMax <= traceDistMin) { return false; }
 
-        double t0Y = std::fmin((MinBound.Y - ray.Origin.Y) / ray.Direction.Y,
-                              (MaxBound.Y - ray.Origin.Y) / ray.Direction.Y);
-        double t1Y = std::fmax((MinBound.Y - ray.Origin.Y) / ray.Direction.Y,
-                              (MaxBound.Y - ray.Origin.Y) / ray.Direction.Y);
-        traceDistMin = std::fmax(t0Y, traceDistMin);
-        traceDistMax = std::fmin(t1Y, traceDistMax);
-        if (traceDistMin >= traceDistMax) { return false; }
+        double invDY = 1.0f / ray.Direction.Y;
+        double t0Y = (MinBound.Y - ray.Origin.Y) * invDY;
+        double t1Y = (MaxBound.Y - ray.Origin.Y) * invDY;
+        if (invDY < 0.0f) { std::swap(t0Y, t1Y); }
+        traceDistMin = t0Y > traceDistMin ? t0Y : traceDistMin;
+        traceDistMax = t1Y < traceDistMax ? t1Y : traceDistMax;
+        if (traceDistMax <= traceDistMin) { return false; }
 
-        double t0Z = std::fmin((MinBound.Z - ray.Origin.Z) / ray.Direction.Z,
-                              (MaxBound.Z - ray.Origin.Z) / ray.Direction.Z);
-        double t1Z = std::fmax((MinBound.Z - ray.Origin.Z) / ray.Direction.Z,
-                              (MaxBound.Z - ray.Origin.Z) / ray.Direction.Z);
-        traceDistMin = std::fmax(t0Z, traceDistMin);
-        traceDistMax = std::fmin(t1Z, traceDistMax);
-        if (traceDistMin >= traceDistMax) { return false; }
+        double invDZ = 1.0f / ray.Direction.Z;
+        double t0Z = (MinBound.Z - ray.Origin.Z) * invDZ;
+        double t1Z = (MaxBound.Z - ray.Origin.Z) * invDZ;
+        if (invDZ < 0.0f) { std::swap(t0Z, t1Z); }
+        traceDistMin = t0Z > traceDistMin ? t0Z : traceDistMin;
+        traceDistMax = t1Z < traceDistMax ? t1Z : traceDistMax;
+        if (traceDistMax <= traceDistMin) { return false; }
 
         return true;
     }
