49b67bc11d68bcb8dad40ffa10b737e15730e763
DeLaPF <de.alafia@gmail.com>
2023-03-04 05:35:15 +0000

    inline faster than static

--- src/Math/Random.h
+++ src/Math/Random.h
@@ -3,7 +3,7 @@
 #include <random>
 
 namespace Random {
-    static double Double() {
+    inline double Double() {
         std::uniform_real_distribution<double> distribution(0.0, 1.0);
         thread_local std::mt19937 generator(std::random_device{}());
         return distribution(generator);
